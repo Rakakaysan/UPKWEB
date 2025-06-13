19cb3ca30a8c5d9f5743f0711e0009d7a0ddebc9
aran8276 <aran8276@gmail.com>
2025-06-13 10:06:47 +0700

    casing

--- app/Http/Controllers/KompetisiController.php
+++ app/Http/Controllers/KompetisiController.php
@@ -6,7 +6,8 @@ use Illuminate\Http\Request;
 
 class KompetisiController extends Controller
 {
-    public function kompetisi () {
-     return view('public.kompetisi');
+    public function kompetisi()
+    {
+        return view('public.kompetisi');
     }
 }
